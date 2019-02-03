7f133d6ebbc092e55b6d71f71a16673ca4ef32f7
AlonGoltzman <alon.goltzman.com>
2019-02-03 23:44:12 +0200

    Initial state save
    Started to work on a state system, to track the player's points.

---
+++ Eclipse Assault/Assets/Scripts/Management/State.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mgmt
+{
+    public class State
+    {
+
+        /// <summary>
+        /// The player's current amount of points.
+        /// </summary>
+        private int CurrentPoints;
+
+        /// <summary>
+        /// The player's toatl points since he started to play, includes the points wasted.
+        /// </summary>
+        private int TotalPoints;
+
+        /// <summary>
+        /// A placeholder.
+        /// Later on the store will be introduced and the player will be able to purchase upgrades.
+        /// These upgrades will be saved in the state.
+        /// </summary>
+        private object[] PurchasedUpgrades;
+
+        /// <summary>
+        /// Creates a new default state that is used to define that a new "save" is created.
+        /// </summary>
+        /// <returns></returns>
+        public static State NewState()
+        {
+            return new State(0, 0, null);
+        }
+
+        public State(int currentPoints, int totalPoints, object[] purchasedUpgrades)
+        {
+            CurrentPoints = currentPoints;
+            TotalPoints = totalPoints;
+            PurchasedUpgrades = purchasedUpgrades;
+        }
+
+        public int Points { get { return CurrentPoints; } }
+        public int HistoricPoints { get { return TotalPoints; } }
+        
+    }
+}
